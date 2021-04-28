2b4aa695a0e18c2bc8d1e7aaf2ba972faf12401b
Chelsey Morris <chelseymorris91@yahoo.com>
2021-04-28 11:48:59 -0700

    deploy

---
+++ google/client/build/precache-manifest.a883323fc3706277a7ebe5f4586b45ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "931a3131a19df57992f102f980c1d009",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a062398bc79f1333a1",
+    "url": "/static/css/main.33e6fd29.chunk.css"
+  },
+  {
+    "revision": "b54b35988f847c8fb4de",
+    "url": "/static/js/2.7bf1cf89.chunk.js"
+  },
+  {
+    "revision": "9be8f09f0bfe1d20164d4c42e389b466",
+    "url": "/static/js/2.7bf1cf89.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a062398bc79f1333a1",
+    "url": "/static/js/main.f16290db.chunk.js"
+  },
+  {
+    "revision": "816cf679577cc3650d0c",
+    "url": "/static/js/runtime-main.87c4f50d.js"
+  }
+]);
